d794740aa3fd339dfd33a8acd6d8994f4c72b759
M-H-BUTT <63429679+M-H-BUTT@users.noreply.github.com>
2020-11-17 16:27:36 +0500

    adding logo into app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import pic from './logo.svg' ;
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Happy Hacking Bro
+      <img src = {pic} className = "App-logo" alt = "main logo here" />
     </div>
   );
 }
