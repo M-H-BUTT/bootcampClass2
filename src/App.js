e49badb4d5520162dbc508bbc9a2eb9826fd53fa
M-H-BUTT <63429679+M-H-BUTT@users.noreply.github.com>
2020-11-18 20:36:29 +0500

    comenting app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import pic from './logo.svg' ;
-import './App.css';
+// // import pic from './logo.svg' ;
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Happy Hacking Bro
-      <hr/>
-      <img src = {pic} className = "App-logo" alt = "main logo here" />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       Happy Hacking Bro
+//       <hr/>
+//       {/* <img src = {pic} className = "App-logo" alt = "main logo here" /> */}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
